f04223625ad6e34ca4c88a1a2455c394ba963c94
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:35:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// question 14/ guest list
+var guest_list = ["washo dev", "sham", "fatima", "kailas", "usman"];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Dear ".concat(guest_list[i], "\n\nyou are invited for a dinner tomorrow,\n\nThank you,"));
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+// question 14/ guest list
+
+let guest_list: string[] = ["washo dev","sham","fatima","kailas","usman"];
+for(let i=0; i<guest_list.length; i++){
+    console.log(`Dear ${guest_list[i]}\n\nyou are invited for a dinner tomorrow,\n\nThank you,`);
+}
+
